d2e416695a8df0ccfcd83abd7e399de1f135bc7d
DipadityaDas <DipadityaDas@gmail.com>
2024-04-04 11:18:09 +0530

    Minor Changes

--- Termination.py
+++ Termination.py
@@ -15,45 +15,45 @@ def create_excel_sheet() -> Worksheet:
 
 
 def cell_style(sheet: Worksheet) -> None:
-	
+
 	for row in sheet.iter_rows():
 		for cell in row:
 			cell.alignment = Alignment(horizontal='center', vertical='center')
 			cell.font = Font(name='Cascadia Code')
-	
+
 	for column in sheet.columns:
 		max_length = max(len(str(cell.value)) for cell in column)
 		adjusted_width = (max_length + 2) * 1.2
 		sheet.column_dimensions[column[0].column_letter].width = adjusted_width
-	
+
 	sheet.sheet_view.zoomScale = 180
 
 
 def group_users(reports: list) -> None:
 	print("All Termination Reports")
 	print("-" * 70)
-	
+
 	for idx, file in enumerate(reports, start=1):
 		print(f"{idx}.  {file}")
-	
+
 	print("=" * 70)
-	
+
 	data_frames = []
 	for report in reports:
 		df = pd.read_excel(dir_path + report, skiprows=1)
 		data_frames.append(df)
-	
+
 	merged_column = pd.concat([df.iloc[:, 0] for df in data_frames], ignore_index=True)
 	print("[INFO] Merged all the User Login IDs.")
-	
+
 	sorted_unique_column = sorted(merged_column.unique())
 	print("[INFO] Deleted all duplicate User Login IDs and sorted the IDs.")
-	
+
 	for idx, data in enumerate(sorted_unique_column, start=2):
 		ws.cell(row=idx, column=2).value = data
-	
+
 	cell_style(ws)
-	
+
 	print(f"[INFO] Found {len(sorted_unique_column)} User Login IDs.")
 	print("[INFO] Successfully created the group.xlsx file.")
 
@@ -68,9 +68,9 @@ if __name__ == '__main__':
 	dir_path = "C:\\Users\\Dipaditya\\Downloads\\"
 	excel_file = 'group.xlsx'
 	csv_file = "Terminate_Users.csv"
-	
-	file_paths = [key for key in os.listdir(dir_path) if key.__contains__('VF')]
-	
+
+	file_paths = [key for key in os.listdir(dir_path) if key.__contains__('Analytics')]
+
 	print("=" * 70)
 	print("Generating Log....")
 	print("-" * 70)
@@ -85,4 +85,3 @@ if __name__ == '__main__':
 	else:
 		print('[INFO] No termination report found.')
 
-	
